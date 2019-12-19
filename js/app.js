dbaeddfd1d34eed727331f23d3f9b08210790bb8
Darrion Goggins <52177451+Darrion-goggins@users.noreply.github.com>
2019-12-19 15:23:33 -0600

    Added DOM manipulation, need to style page

--- js/app.js
+++ js/app.js
@@ -2,11 +2,12 @@ function ageComparisson() {
     var dob = document.getElementById("dateinfo").value;
     var birthdate = new Date(dob);
     var currentdate = new Date();
-    var agerestriction = Math.floor(currentdate - birthdate); // used to convert into actual age
+    const agerestriction = Math.floor(currentdate - birthdate); // used to convert into actual age
+    let output = document.getElementById("output");
 
     if(agerestriction > 568025136000) {
-        console.log("You are ", Math.floor(agerestriction/31536000000), "years old, you may pass");
+        output.innerText = `You are ${Math.floor(agerestriction/31536000000)}, years old, you may pass`;
     } else {
-        console.log("You may not enter, you are", Math.floor(agerestriction/31536000000), "years old and are too young.");
+        output.innerText = `You may not enter, you are ${Math.floor(agerestriction/31536000000)}, years old and are too young.`;
     }
 }
